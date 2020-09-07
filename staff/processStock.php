8efbcb5dac3b6fcb3245f7a08bb7a69a90c0146c
Adriana Azlan <adrnzln@gmail.com>
2020-09-07 23:26:19 +0800

    Stock - Functions (except update)

---
+++ staff/processStock.php
@@ -0,0 +1,28 @@
+<?php
+include "stock.php";
+if(isSet($_POST['addMenuButton']))
+	{
+	header('Location: stockInfo.php');
+	}
+if(isSet($_POST['saveNewMenuButton']))
+	{
+	addNewMenu();
+	header( "refresh:1; url=StaffStock.php" );
+	}
+if(isSet($_POST['deleteMenuButton']))
+	{
+	deleteMenu();
+	echo "<script>";
+	echo " alert('Menu Record has been deleted.');
+		</script>";
+	header( "refresh:1; url=stock.php" );
+	}
+if(isSet($_POST['updateMenuButton']))
+	{
+	updateMenuInformation();
+	header( "refresh:1; url=stock.php" );
+	}
+
+
+
+?>
