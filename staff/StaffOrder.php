d773411debb90326cef6f25d542708e7e071088a
Adriana Azlan <adrnzln@gmail.com>
2020-09-08 10:55:15 +0800

    Staff Order - all functions working (except update, delete order)

--- staff/StaffOrder.php
+++ staff/StaffOrder.php
@@ -300,6 +300,13 @@
   }
 </style>
 
+<script>
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+});
+</script>
+</head>
+
 <!-- Navbar -->
 <nav class="navbar navbar-default">
   <div class="container">
@@ -322,123 +329,74 @@
   </div>
 </nav>
 
-<script>
-$(document).ready(function(){
-	$('[data-toggle="tooltip"]').tooltip();
-});
-</script>
-</head>
-<body>
-    <div class="container">
-        <div class="table-responsive">
-            <div class="table-wrapper">
-                <div class="table-title">
-                    <div class="row">
-                        <div class="col-sm-8"><h2><b>Order</b> Details</h2></div>
-                        <div class="col-sm-4">
-                            <div class="search-box">
-                                <i class="material-icons">&#xE8B6;</i>
-                                <input type="text" class="form-control" placeholder="Search&hellip;">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <table class="table table-striped table-hover table-bordered">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name <i class="fa fa-sort"></i></th>
-                            <th>Address</th>
-                            <th>City <i class="fa fa-sort"></i></th>
-                            <th>Pin Code</th>
-                            <th>Country <i class="fa fa-sort"></i></th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Thomas Hardy</td>
-                            <td>89 Chiaroscuro Rd.</td>
-                            <td>Portland</td>
-                            <td>97219</td>
-                            <td>USA</td>
-                            <td>
-                                <a href="#" class="view" title="View" data-toggle="tooltip"><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Maria Anders</td>
-                            <td>Obere Str. 57</td>
-                            <td>Berlin</td>
-                            <td>12209</td>
-                            <td>Germany</td>
-                            <td>
-                                <a href="#" class="view" title="View" data-toggle="tooltip"><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Fran Wilson</td>
-                            <td>C/ Araquil, 67</td>
-                            <td>Madrid</td>
-                            <td>28023</td>
-                            <td>Spain</td>
-                            <td>
-                                <a href="#" class="view" title="View" data-toggle="tooltip"><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>Dominique Perrier</td>
-                            <td>25, rue Lauriston</td>
-                            <td>Paris</td>
-                            <td>75016</td>
-                            <td>France</td>
-                            <td>
-                                <a href="#" class="view" title="View" data-toggle="tooltip"><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>Martin Blank</td>
-                            <td>Via Monte Bianco 34</td>
-                            <td>Turin</td>
-                            <td>10100</td>
-                            <td>Italy</td>
-                            <td>
-                                <a href="#" class="view" title="View" data-toggle="tooltip"><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>        
-                    </tbody>
-                </table>
-                <div class="clearfix">
-                    <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
-                    <ul class="pagination">
-                        <li class="page-item disabled"><a href="#"><i class="fa fa-angle-double-left"></i></a></li>
-                        <li class="page-item"><a href="#" class="page-link">1</a></li>
-                        <li class="page-item"><a href="#" class="page-link">2</a></li>
-                        <li class="page-item active"><a href="#" class="page-link">3</a></li>
-                        <li class="page-item"><a href="#" class="page-link">4</a></li>
-                        <li class="page-item"><a href="#" class="page-link">5</a></li>
-                        <li class="page-item"><a href="#" class="page-link"><i class="fa fa-angle-double-right"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>        
-    </div>     
-</body>
+	<div class="container-fluid bg-1 text-center">
+	<img src="fm2go-ICON.png" class="img-responsive margin" style="display:inline" alt="Bird" width="350" height="350">
+	</div>
+<?php
+	include "order.php";
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1); 
+	error_reporting(E_ALL);
+
+	$qry = getListOfMenu();
+	echo '<form action = "processOrder.php" method ="POST">';
+	//echo '<br><input type = "submit" name="addMenuButton" value ="New Order" class="center-block" style="color:black">';
+	
+	echo '</form>';
+	echo '<br>';
+	echo '<div class="container-lg">
+    <div class="table-responsive">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+			         <div class="col-sm-8"><h3>Order <b>Details</b></h3></div>
+				<h4 class="card-title" align="center">  </h4>
+			</div>
+			<div class="card-body">
+			<div class="table-responsive">
+            <table class="table">
+				<thead class=" text-primary">
+					<th>No</th>
+					<th>Id</th>
+					<th>Name</th>
+					<th>Quantity</th>
+					<th>Price</th>
+					<th>Action</th>
+				</thead>';
+	$i=1;
+	while($row=mysqli_fetch_assoc($qry))//Display menu information
+	{
+		echo '<tbody>';
+		echo '<tr>';
+		echo '<td>'.$i.'</td>';
+		echo '<td>'.$row['Id'].'</td>';
+		echo '<td>'.$row['Name'].'</td>';
+		echo '<td>'.$row['Quantity'].'</td>';
+		echo '<td>'.$row['Price'].'</td>';
+		$orderId = $row['Id'];
+				echo '<td>';
+			echo '<form style="display:inline-block" action="updateOrderForm.php" method="post" >';
+			echo "<input type='hidden' value='$orderId' name='menuNameToUpdate'>";
+			echo '<button type="submit" name="updateMenuButton" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i> </button>';
+			echo '</form>';
+			echo '<form style="display:inline-block" action="processOrder.php" method="post" >';
+			echo "<input type='hidden' value='$orderId' name='menuNameToDelete'>";
+			echo '<button type="submit" name="deleteMenuButton" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i> </button>';
+			echo '</form>';
+		echo '</td>';
+		//delete menu
+		$i++;
+	}
+		echo'</tr>
+        </tbody>
+        </table>
+        </div>
+        </div>
+        </div>
+        </div>';
+?></div>
+	
 <!-- Footer -->
 <footer class="site-footer">
       <div class="container">
